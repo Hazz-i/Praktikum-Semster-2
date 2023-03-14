63c75e5b78b221a4daf2df350db3f84ad18d8dce
Wahid Hasim <wahidh697@gmail.com>
2023-03-15 06:45:44 +0700

    Materi Praktik dan Tugas 1

---
+++ Praktikum1/ConsoleApp1/praktikum1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    class praktikum1
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Input A: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input B: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil dari penjumlahan {0} + {1} = {2}", a, b, penjumlahan(a, b));
+            Console.WriteLine("Hasil dari pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+            Console.WriteLine("Hasil dari perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+            Console.WriteLine("Hasil dari pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+
+            Console.WriteLine("\nTekan Untuk Keluar");
+            Console.ReadKey();
+        }
+
+        static int penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Praktikum1/Tugas1/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorConsoleApp
+{
+    class praktikum1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t\tMenu");
+            Console.WriteLine("1. Penjumlahan");
+            Console.WriteLine("2. Pengurangan");
+            Console.WriteLine("3. Perkalian");
+            Console.WriteLine("4. Pembagian");
+            Console.Write("\nPilih Menu: ");
+            int menu = Convert.ToInt32(Console.ReadLine()); ;
+
+            if (menu > 4 || menu < 1)
+            {
+                Console.WriteLine("Data Not Found");
+            }
+            else
+            {
+                switch (menu)
+                {
+                    case 1:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\t\tPenjumlahan");
+                            Console.Write("Input A: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Input B: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("\n\nHasil dari penjumlahan {0} + {1} = {2}", a, b, penjumlahan(a, b));
+                            Console.ReadKey();
+
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\t\tPengurangan");
+                            Console.Write("Input A: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Input B: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("\n\nHasil dari pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+                            Console.ReadKey();
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\t\tPerkalian");
+                            Console.Write("Input A: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Input B: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("\n\nHasil dari perkalian {0} * {1} = {2}", a, b, perkalian(a, b));
+                            Console.ReadKey();
+
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.Clear();
+                            Console.WriteLine("\t\tPembagian");
+                            Console.Write("Input A: ");
+                            int a = Convert.ToInt32(Console.ReadLine());
+                            Console.Write("Input B: ");
+                            int b = Convert.ToInt32(Console.ReadLine());
+
+                            Console.WriteLine("\n\nHasil dari pembagian {0} / {1} = {2}", a, b, pembagian(a, b));
+                            Console.ReadKey();
+
+                            break;
+                        }
+                }
+            }
+        }
+
+        static int penjumlahan(int a, int b)
+        {
+            return a + b;
+        }
+        static int pengurangan(int a, int b)
+        {
+            return a - b;
+        }
+        static int perkalian(int a, int b)
+        {
+            return a * b;
+        }
+        static int pembagian(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
